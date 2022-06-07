605eade74563504fecd407b8b29aa32cedc79a28
bitlatte <github@bitlatte.io>
2022-06-06 23:21:32 -0500

    Fixed Zip Extraction

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,6 @@ import * as path from 'path';
 import { Extract, Parse } from 'unzipper';
 
 const url = 'https://codeload.github.com/Bitlatte/vite-react/zip/main';
-// @ts-ignore
 const args = process.argv.slice(2);
 
 const fetch = (url: string) => {
@@ -23,19 +22,27 @@ const fetch = (url: string) => {
   })
 }
 
-const unzip = (file: string, path: string) => {
-  fs.createReadStream(file).pipe(Extract({ path: path }));
+const extract = (stream: fs.ReadStream, path: string) => {
+  return new Promise<void>((resolve) => {
+    stream
+      .pipe(Extract({ path: path }))
+      .on('close', () => {
+        resolve();
+      })
+  })
 }
 
 fetch(url)
   .then(() => {
-    const zip = path.join(process.cwd(), 'repo.zip');
-    unzip(zip, process.cwd());
-    fs.rename(`${process.cwd()}/vite-react-main`, `${process.cwd()}/${args[0]}`, (err) => {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log('Done');
-        }
-      })
+    const stream = fs.createReadStream(path.join(process.cwd(), 'repo.zip'));
+    extract(stream, process.cwd())
+      .then(() => {
+        fs.rename(`${process.cwd()}/vite-react-main`, `${process.cwd()}/${args[0]}`, (err) => {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log('Done');
+          }
+        })
+      });
   }).catch(e => console.log(e));
